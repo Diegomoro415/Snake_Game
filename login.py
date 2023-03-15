3fc90ef0a36d76890e1a081872d33a5436585a91
Diegomoro415 <diego_moro415@hotmail.com>
2023-03-15 20:57:15 +0100

    add JSON file, add icon img and create class login and initiate the main window

--- login.py
+++ login.py
@@ -8,6 +8,25 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    'service_account.json', scope)
+    'credssnakegame.json', scope)
 gc = gspread.authorize(credentials)
 wks = gc.open('snakegame_creds').sheet1
+
+# Setting Login interface with Tkinter
+class Login(tk.Frame):
+    def __init__(self, master):
+        """
+        Constructor for Login class, initializes and configures the login window
+
+        Args:
+            master: A Tkinter object, which is the parent window
+        """
+        # Create the main window
+        super().__init__(master)
+        self.master = master
+        self.master.title("SnakeGame Login")
+        self.master.geometry("500x500+511+150")
+        self.master.wm_resizable(width=False, height=False)
+        self.master.iconbitmap(default='snake.ico')
+
+    
